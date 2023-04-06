7a7449f4e657b6005d1e84e8944aa305e7f65482
Dioneer <Gulkina_em@mail.ru>
2023-04-06 22:56:24 +0300

    normolise Example2

--- Exemple2/Program.cs
+++
@@ -1,5 +0,0 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.Write("Введите имя ");
-string username = Console.ReadLine();
-Console.Write("Привет, ");
-Console.Write("Привет, ");
