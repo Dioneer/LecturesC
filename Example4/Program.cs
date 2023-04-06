dae7ed10fbd8bf15b94166458e1fd12791532fa6
Dioneer <Gulkina_em@mail.ru>
2023-04-06 23:39:28 +0300

    normolise Example4

--- Example4/Program.cs
+++ Example4/Program.cs
@@ -1,4 +1,16 @@
-﻿// See https://aka.ms/new-console-template for more information
-double numberA = 12;
-double nubmerB = 5;
-Console.WriteLine(numberA / nubmerB);
+﻿using System;
+
+namespace Example4
+{
+	class Program4
+	{
+		static void Main()
+		{
+			float numberA = 12, nubmerB = 5;
+			short numberC;
+			numberC = Convert.ToInt16(Console.ReadLine());
+			Console.WriteLine(numberA / nubmerB);
+			Console.WriteLine(numberC);
+		}
+	}
+}
