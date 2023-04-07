70af8e08a7caa1b5897e5f541fcfb1cf40392b9c
Dioneer <Gulkina_em@mail.ru>
2023-04-07 10:23:52 +0300

    normolise Example7

---
+++ Example2/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace Example2
+{
+	class Program2
+	{
+		static void Main()
+		{
+			Console.Write("Введите имя: ");
+			string username = Console.ReadLine();
+			Console.Write("Привет, ");
+			Console.Write(username + "!");
+			Console.ReadKey();
+		}
+	}
+}
