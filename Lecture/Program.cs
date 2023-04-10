405c5c42413e1a0b56cf1c569ea73f48a45e7627
Dioneer <Gulkina_em@mail.ru>
2023-04-10 13:27:43 +0300

    relocation code from first lecture

---
+++ Lecture/Program.cs
@@ -0,0 +1,106 @@
+﻿using System;
+
+namespace Example1
+{
+	class Program
+	{
+		static void Main()
+		{
+			//Example1
+			// Console.WriteLine("Hello, World!");
+			//Example2
+			// Console.Write("Введите имя: ");
+			// string username = Console.ReadLine();
+			// Console.Write("Привет, ");
+			// Console.Write(username + "!");
+			// Console.ReadKey();
+			//Example3
+			// int numberA = new Random().Next(1, 10);
+			// int nubmerB = new Random().Next(1, 10);
+			// byte numberC = 12;
+			// float numberD = 1.236545f;
+			// numberC += 4;
+			// int result = numberA + nubmerB;
+			// Console.WriteLine("result: " + result);
+			// Console.WriteLine("numberC: " + numberC);
+			// Console.WriteLine("float: " + numberD);
+			//Example4
+			// float numberA = 12, nubmerB = 5, numberC;
+			// numberC = float.Parse(Console.ReadLine());
+			// Console.WriteLine(numberA / nubmerB);
+			// Console.WriteLine(numberC);
+			//Example5
+			// Console.Write("Enter Name: ");
+			// string username = Console.ReadLine();
+
+			// if (username.ToLower() == "Masha")
+			// {
+			// 	Console.WriteLine("Heeeeeeyyyy");
+			// }
+			// else
+			// {
+			// 	Console.WriteLine($"Hello, {username}");
+			// }
+			// Console.Write("Write a float number, use coma for separate: ");
+			// float user_name = float.Parse(Console.ReadLine());
+			// float result = user_name + 10f;
+			// System.Console.WriteLine($"Your number + 10 = {result}");
+			//Example6
+			// int a = 1;
+			// int b = 2;
+			// int c = 6;
+			// int e = 8;
+
+			// int max = a;
+
+			// if (b > max) max = b;
+			// if (c > max) max = c;
+			// if (e > max) max = e;
+
+			// Console.Write($"max ={max}");
+			//Example7
+			//Console.Clear();
+
+			// int xa = 40;
+			// int ya = 1;
+			// int xb = 1;
+			// int yb = 30;
+			// int xc = 80;
+			// int yc = 30;
+			// int x = xa, y = ya;
+
+			// Console.SetCursorPosition(xa, ya);
+			// Console.WriteLine("+");
+
+			// Console.SetCursorPosition(xb, yb);
+			// Console.WriteLine("+");
+
+			// Console.SetCursorPosition(xc, yc);
+			// Console.WriteLine("+");
+
+			// short count = 10000;
+			// while (count > 0)
+			// {
+			// 	int what = new Random().Next(0, 3);
+			// 	if (what == 0)
+			// 	{
+			// 		x = (x + xa) / 2;
+			// 		y = (y + ya) / 2;
+			// 	}
+			// 	if (what == 1)
+			// 	{
+			// 		x = (x + xb) / 2;
+			// 		y = (y + yb) / 2;
+			// 	}
+			// 	if (what == 2)
+			// 	{
+			// 		x = (x + xc) / 2;
+			// 		y = (y + yc) / 2;
+			// 	}
+			// 	Console.SetCursorPosition(x, y);
+			// 	Console.WriteLine("+");
+			// 	count--;
+			// }
+		}
+	}
+}
